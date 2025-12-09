da715762010d4b57bc4b274b05f4d5588e6a2f30
tienpham <tpha768@gmail.com>
2025-12-09 20:18:33 +0700

    add: add cors lib

--- be-express/server.js
+++ be-express/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
 
 mongoose
   .connect("mongodb://localhost:27017/demo_cloudcomputing")
