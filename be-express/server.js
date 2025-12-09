41b06cf14b474e934b2daf03017b8e286ecac049
tienpham <tpha768@gmail.com>
2025-12-09 17:56:12 +0700

    update: update del connect db

--- be-express/server.js
+++ be-express/server.js
@@ -2,14 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-mongoose
-  .connect("mongodb://localhost:27017/demo_cloudcomputing")
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log("Error connecting to MongoDB", err);
-  });
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
