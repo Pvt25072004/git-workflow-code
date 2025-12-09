b55265846774eba02414d8d82ed151e52df4ac16
tienpham <tpha768@gmail.com>
2025-12-09 18:12:05 +0700

    revert main mmmssdsdd

--- be-express/server.js
+++ be-express/server.js
@@ -10,5 +10,5 @@ mongoose
     console.log("Error connecting to MongoDB", err);
   });
 app.listen(8080, () => {
-  console.log("Server is running on port 8080 kk");
+  console.log("Server is running on port 8080 kkae");
 });
